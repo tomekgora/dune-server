67f12c0fc0797cbe95232b902d7bacf030bb7ac6
tomekgora <tomekgora@gmail.com>
2020-02-22 20:17:22 +0100

    changed traitor deck to leaders

---
+++ src/game-data/Leaders.js
@@ -0,0 +1,17 @@
+const Leaders = [
+  { id: 1, traitorName: "Bashar" },
+  { id: 2, traitorName: "Burseg" },
+  { id: 3, traitorName: "Caid" },
+  { id: 4, traitorName: "Captain Aramsham" },
+  { id: 5, traitorName: "Hasimir Fenring" },
+  { id: 6, traitorName: "Guild Representative" },
+  { id: 7, traitorName: "Soo-Soo Sook" },
+  { id: 8, traitorName: "Esmar Tuek" },
+  { id: 9, traitorName: "Master Bewt" },
+  { id: 10, traitorName: "Staban Tuek" },
+  { id: 11, traitorName: "Jamis" },
+  { id: 12, traitorName: "Shadout Mapes" },
+  { id: 13, traitorName: "Otheym" }
+];
+
+export default traitorDeck;
--- src/game-data/traitor-deck.js
+++
@@ -1,3 +0,0 @@
-const traitorDeck = [];
-
-export default traitorDeck;
