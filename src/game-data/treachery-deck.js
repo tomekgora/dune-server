19da2421722bdba069568f7f0e39394090e61ad8
tomekgora <tomekgora@gmail.com>
2020-02-22 20:54:15 +0100

    expanded treachery deck

--- src/game-data/treachery-deck.js
+++ src/game-data/treachery-deck.js
@@ -1,3 +1,112 @@
-const treacheryDeck = [];
+const treacheryDeck = [
+  {
+    id: 1,
+    cardName: "Lasgun",
+    cardType: "Weapon - Special",
+    cardDescription: ""
+  },
+  {
+    id: 2,
+    cardName: "Crysknife",
+    cardType: "Weapon - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 3,
+    cardName: "Maula Pistol",
+    cardType: "Weapon - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 4,
+    cardName: "Slip Tip",
+    cardType: "Weapon - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 5,
+    cardName: "Stunner",
+    cardType: "Weapon - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 6,
+    cardName: "Chaumas",
+    cardType: "Weapon - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 7,
+    cardName: "Chaumurky",
+    cardType: "Weapon - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 8,
+    cardName: "Ellaca Dru",
+    cardType: "Weapon - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 9,
+    cardName: "Gom Jabbar",
+    cardType: "Weapon - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 10,
+    cardName: "Shield",
+    cardType: "Defense - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 11,
+    cardName: "Shield",
+    cardType: "Defense - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 12,
+    cardName: "Shield",
+    cardType: "Defense - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 13,
+    cardName: "Shield",
+    cardType: "Defense - Projectile",
+    cardDescription: ""
+  },
+  {
+    id: 14,
+    cardName: "Snooper",
+    cardType: "Defense - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 15,
+    cardName: "Snooper",
+    cardType: "Defense - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 16,
+    cardName: "Snooper",
+    cardType: "Defense - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 17,
+    cardName: "Snooper",
+    cardType: "Defense - Poison",
+    cardDescription: ""
+  },
+  {
+    id: 18,
+    cardName: "Cheap Hero",
+    cardType: "Special - Leader",
+    cardDescription: ""
+  }
+];
 
 export default treacheryDeck;
